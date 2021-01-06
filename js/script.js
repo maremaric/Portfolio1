4e762064165746ea4a18b09909a157f03a1a8dda
maremaric <markoreus888@gmail.com>
2021-01-06 19:13:23 +0100

    vezba

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+/*-------------------------- About setion tabs ------------------------*/
+
+(() => {
+    const aboutSection = document.querySelector(".about__section"),
+    tabsContainer = document.querySelector(".about__tabs");
+
+    tabsContainer.addEventListener("click", (event) => {
+        /* if event.target contains 'tab__item' class and not contains 'active' class */
+        if(event.target.classList.contains("tab__item") && !event.target.classList.contains("active")) {
+            // console.log("event.target contains 'tab__item class and not contains 'active' class");
+            // console.log(event.target);
+            const target = event.target.getAttribute("data-target");
+            // Deactivate existing active 'tab__item'
+            tabsContainer.querySelector(".active").classList.remove("outer__shadow","active");
+            // active new 'tab__item'
+            event.target.classList.add("active","outer__shadow");
+            // deactivate existing active 'tab__content'
+            aboutSection.querySelector(".tab__content.active").classList.remove("active");
+            // active new 'tab__content'
+            aboutSection.querySelector(target).classList.add("active");
+        }
+    })
+})();
+
